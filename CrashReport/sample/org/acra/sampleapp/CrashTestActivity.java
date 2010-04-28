f68dfdc86d88cc6ead638e1e38243b1334c9b597
Kevin Gaudin <kevin.gaudin@gmail.com>
2010-04-28 23:01:27 +0000

    Suppress warning on null value, the code of this crash test is explicit enough.Now there is no warning at all in the project \o/

--- CrashReport/sample/org/acra/sampleapp/CrashTestActivity.java
+++ CrashReport/sample/org/acra/sampleapp/CrashTestActivity.java
@@ -8,6 +8,7 @@ public class CrashTestActivity extends Activity {
     /* (non-Javadoc)
      * @see android.app.Activity#onCreate(android.os.Bundle)
      */
+    @SuppressWarnings("null")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
