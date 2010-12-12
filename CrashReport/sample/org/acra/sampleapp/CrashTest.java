06bc0b283776c07bef2707c74c7020246ca55a6b
Kevin Gaudin <kevin.gaudin@gmail.com>
2010-12-12 21:59:16 +0000

    Get rid of old commentend out code.

--- CrashReport/sample/org/acra/sampleapp/CrashTest.java
+++ CrashReport/sample/org/acra/sampleapp/CrashTest.java
@@ -23,7 +23,7 @@ import org.acra.annotation.ReportsCrashes;
 import android.app.Application;
 
 @ReportsCrashes(formKey = "dEhXSldocldoUDBzR25jM2dtSGo2QXc6MQ",
-        mode = ReportingInteractionMode.TOAST,
+        mode = ReportingInteractionMode.NOTIFICATION,
         resToastText = R.string.crash_toast_text,
         resNotifTickerText = R.string.crash_notif_ticker_text,
         resNotifTitle = R.string.crash_notif_title,
@@ -43,33 +43,4 @@ public class CrashTest extends Application {
         super.onCreate();
     }
 
-    // private Bundle getToastCrashResources() {
-    // Bundle result = new Bundle();
-    // result.putInt(RES_TOAST_TEXT, R.string.crash_toast_text);
-    // return result;
-    // }
-    //
-    // private Bundle getFullNotificationCrashResources() {
-    // Bundle result = new Bundle();
-    // result.putInt(RES_NOTIF_ICON, android.R.drawable.stat_notify_error);
-    // result.putInt(RES_NOTIF_TICKER_TEXT, R.string.crash_notif_ticker_text);
-    // result.putInt(RES_NOTIF_TITLE, R.string.crash_notif_title);
-    // result.putInt(RES_NOTIF_TEXT, R.string.crash_notif_text);
-    // result.putInt(RES_DIALOG_ICON, android.R.drawable.ic_dialog_info);
-    // result.putInt(RES_DIALOG_TITLE, R.string.crash_dialog_title);
-    // result.putInt(RES_DIALOG_TEXT, R.string.crash_dialog_text);
-    // result.putInt(RES_DIALOG_COMMENT_PROMPT,
-    // R.string.crash_dialog_comment_prompt);
-    // result.putInt(RES_DIALOG_OK_TOAST, R.string.crash_dialog_ok_toast);
-    // return result;
-    // }
-    //
-    // private Bundle getMinNotificationCrashResources() {
-    // Bundle result = new Bundle();
-    // result.putInt(RES_NOTIF_TICKER_TEXT, R.string.crash_notif_ticker_text);
-    // result.putInt(RES_NOTIF_TITLE, R.string.crash_notif_title);
-    // result.putInt(RES_NOTIF_TEXT, R.string.crash_notif_text);
-    // result.putInt(RES_DIALOG_TEXT, R.string.crash_dialog_text);
-    // return result;
-    // }
 }
