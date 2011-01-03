f3c6fe1773a358357dfb5ffc78a8bbf436aa2623
Kevin Gaudin <kevin.gaudin@gmail.com>
2011-01-03 21:13:57 +0000

    Removed 3 duplicate tags

--- CrashReport/src/org/acra/DropBoxCollector.java
+++ CrashReport/src/org/acra/DropBoxCollector.java
@@ -11,8 +11,7 @@ import android.text.format.Time;
 import android.util.Log;
 
 public class DropBoxCollector {
-    private static final String[] SYSTEM_TAGS = { "system_server_anr", "system_server_wtf", "system_server_crash",
-        "system_app_anr", "system_app_wtf", "system_app_crash", "system_server_anr",
+    private static final String[] SYSTEM_TAGS = { "system_app_anr", "system_app_wtf", "system_app_crash", "system_server_anr",
         "system_server_wtf", "system_server_crash", "BATTERY_DISCHARGE_INFO",
         "SYSTEM_RECOVERY_LOG", "SYSTEM_BOOT", "SYSTEM_LAST_KMSG", "APANIC_CONSOLE",
         "APANIC_THREADS", "SYSTEM_RESTART", "SYSTEM_TOMBSTONE", "data_app_strictmode" };
