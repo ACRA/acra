f642b7ae30d78e3dc6fa6888c12a0b21b3427a30
Kevin Gaudin <kevin.gaudin@gmail.com>
2011-02-23 23:39:11 +0000

    Javadoc update

--- CrashReport/src/org/acra/sender/EmailIntentSender.java
+++ CrashReport/src/org/acra/sender/EmailIntentSender.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2010 Emmanuel Astier & Kevin Gaudin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.acra.sender;
 
 import org.acra.ACRA;
@@ -7,6 +22,10 @@ import org.acra.ReportField;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * Send reports throught an email intent. The user will be asked to chose his prefered email client.
+ * Included report fields can be defined using {@link ReportsCrashes#mailReportFields()}.
+ */
 public class EmailIntentSender implements ReportSender {
     Context mContext = null;
 
