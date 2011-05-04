5fc27fa7bf95d8b414f661dfe7012beffbe0bef2
Kevin Gaudin <kevin.gaudin@gmail.com>
2011-05-04 23:17:02 +0000

    Addition of field IS_SILENT in reports

--- acra/src/main/java/org/acra/annotation/ReportsCrashes.java
+++ acra/src/main/java/org/acra/annotation/ReportsCrashes.java
@@ -15,7 +15,40 @@
  */
 package org.acra.annotation;
 
-import static org.acra.ReportField.*;
+import static org.acra.ReportField.ANDROID_VERSION;
+import static org.acra.ReportField.APP_VERSION_CODE;
+import static org.acra.ReportField.APP_VERSION_NAME;
+import static org.acra.ReportField.AVAILABLE_MEM_SIZE;
+import static org.acra.ReportField.BRAND;
+import static org.acra.ReportField.BUILD;
+import static org.acra.ReportField.CRASH_CONFIGURATION;
+import static org.acra.ReportField.CUSTOM_DATA;
+import static org.acra.ReportField.DEVICE_FEATURES;
+import static org.acra.ReportField.DEVICE_ID;
+import static org.acra.ReportField.DISPLAY;
+import static org.acra.ReportField.DROPBOX;
+import static org.acra.ReportField.DUMPSYS_MEMINFO;
+import static org.acra.ReportField.ENVIRONMENT;
+import static org.acra.ReportField.EVENTSLOG;
+import static org.acra.ReportField.FILE_PATH;
+import static org.acra.ReportField.INITIAL_CONFIGURATION;
+import static org.acra.ReportField.INSTALLATION_ID;
+import static org.acra.ReportField.IS_SILENT;
+import static org.acra.ReportField.LOGCAT;
+import static org.acra.ReportField.PACKAGE_NAME;
+import static org.acra.ReportField.PHONE_MODEL;
+import static org.acra.ReportField.PRODUCT;
+import static org.acra.ReportField.RADIOLOG;
+import static org.acra.ReportField.REPORT_ID;
+import static org.acra.ReportField.SETTINGS_SECURE;
+import static org.acra.ReportField.SETTINGS_SYSTEM;
+import static org.acra.ReportField.STACK_TRACE;
+import static org.acra.ReportField.TOTAL_MEM_SIZE;
+import static org.acra.ReportField.USER_APP_START_DATE;
+import static org.acra.ReportField.USER_COMMENT;
+import static org.acra.ReportField.USER_CRASH_DATE;
+import static org.acra.ReportField.USER_EMAIL;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -30,10 +63,9 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 
 /**
- * Provide configuration elemets to the
- * {@link ACRA#init(android.app.Application)} method. The only mandatory
- * configuration item is the {@link #formKey()} parameter which is the Id of
- * your Google Documents form which will receive reports.
+ * Provide configuration elemets to the {@link ACRA#init(android.app.Application)} method. The only mandatory
+ * configuration item is the {@link #formKey()} parameter which is the Id of your Google Documents form which will
+ * receive reports.
  * 
  * @author Kevin Gaudin
  * 
@@ -50,9 +82,8 @@ public @interface ReportsCrashes {
     String formKey();
 
     /**
-     * The Uri of your own server-side script that will receive reports. This is
-     * to use if you don't want to send reports to Google Docs but to your own
-     * script.
+     * The Uri of your own server-side script that will receive reports. This is to use if you don't want to send
+     * reports to Google Docs but to your own script.
      * 
      * @return
      */
@@ -60,19 +91,16 @@ public @interface ReportsCrashes {
 
     /**
      * <p>
-     * The interaction mode you want to implement. Default is
-     * {@link ReportingInteractionMode#SILENT} which does not require any
-     * resources configuration.
+     * The interaction mode you want to implement. Default is {@link ReportingInteractionMode#SILENT} which does not
+     * require any resources configuration.
      * </p>
      * <p>
      * Other modes have resources requirements:
      * <ul>
-     * <li>{@link ReportingInteractionMode#TOAST} requires
-     * {@link #resToastText()} to be provided to define the text that you want
-     * to be displayed to the user when a report is being sent.</li>
-     * <li>{@link ReportingInteractionMode#NOTIFICATION} requires
-     * {@link #resNotifTickerText()}, {@link #resNotifTitle()},
-     * {@link #resNotifText()}, {@link #resDialogText()}</li>
+     * <li>{@link ReportingInteractionMode#TOAST} requires {@link #resToastText()} to be provided to define the text
+     * that you want to be displayed to the user when a report is being sent.</li>
+     * <li>{@link ReportingInteractionMode#NOTIFICATION} requires {@link #resNotifTickerText()},
+     * {@link #resNotifTitle()}, {@link #resNotifText()}, {@link #resDialogText()}</li>
      * </ul>
      * </p>
      * 
@@ -81,14 +109,13 @@ public @interface ReportsCrashes {
     ReportingInteractionMode mode() default ReportingInteractionMode.SILENT;
 
     /**
-     * Resource id for the user comment input label in the crash dialog. If not
-     * provided, disables the input field.
+     * Resource id for the user comment input label in the crash dialog. If not provided, disables the input field.
      */
     int resDialogCommentPrompt() default 0;
 
     /**
-     * Resource id for the user email address input label in the crash dialog.
-     * If not provided, disables the input field.
+     * Resource id for the user email address input label in the crash dialog. If not provided, disables the input
+     * field.
      */
     int resDialogEmailPrompt() default 0;
 
@@ -98,8 +125,7 @@ public @interface ReportsCrashes {
     int resDialogIcon() default android.R.drawable.ic_dialog_alert;
 
     /**
-     * Resource id for the Toast text triggered when the user accepts to send a
-     * report in the crash dialog.
+     * Resource id for the Toast text triggered when the user accepts to send a report in the crash dialog.
      */
     int resDialogOkToast() default 0;
 
@@ -134,25 +160,22 @@ public @interface ReportsCrashes {
     int resNotifTitle() default 0;
 
     /**
-     * Resource id for the Toast text triggered when the application crashes if
-     * the notification+dialog mode is not used.
+     * Resource id for the Toast text triggered when the application crashes if the notification+dialog mode is not
+     * used.
      */
     int resToastText() default 0;
 
     /**
-     * Name of the SharedPreferences that will host the
-     * {@link ACRA#PREF_DISABLE_ACRA} or {@link ACRA#PREF_ENABLE_ACRA}
-     * preference. Default is to use the default SharedPreferences, as retrieved
-     * with {@link PreferenceManager#getDefaultSharedPreferences(Context)}.
+     * Name of the SharedPreferences that will host the {@link ACRA#PREF_DISABLE_ACRA} or {@link ACRA#PREF_ENABLE_ACRA}
+     * preference. Default is to use the default SharedPreferences, as retrieved with
+     * {@link PreferenceManager#getDefaultSharedPreferences(Context)}.
      */
     String sharedPreferencesName() default "";
 
     /**
-     * If using a custom {@link ReportsCrashes#sharedPreferencesName()}, pass
-     * here the mode that you need for the SharedPreference file creation:
-     * {@link Context#MODE_PRIVATE}, {@link Context#MODE_WORLD_READABLE} or
-     * {@link Context#MODE_WORLD_WRITEABLE}. Default is
-     * {@link Context#MODE_PRIVATE}.
+     * If using a custom {@link ReportsCrashes#sharedPreferencesName()}, pass here the mode that you need for the
+     * SharedPreference file creation: {@link Context#MODE_PRIVATE}, {@link Context#MODE_WORLD_READABLE} or
+     * {@link Context#MODE_WORLD_WRITEABLE}. Default is {@link Context#MODE_PRIVATE}.
      * 
      * @see Context#getSharedPreferences(String, int)
      */
@@ -184,21 +207,18 @@ public @interface ReportsCrashes {
     boolean includeDropBoxSystemTags() default false;
 
     /**
-     * You can provide here a list of tags that will be fetched when collecting
-     * DropBox entries.
+     * You can provide here a list of tags that will be fetched when collecting DropBox entries.
      */
     String[] additionalDropBoxTags() default {};
 
     /**
-     * How many minutes will be looked back when collecting events from
-     * DropBoxManager.
+     * How many minutes will be looked back when collecting events from DropBoxManager.
      */
     int dropboxCollectionMinutes() default 5;
 
     /**
      * <p>
-     * Arguments to be passed to the logcat command line. Default is { "-t",
-     * "200", "-v", "time" } for:
+     * Arguments to be passed to the logcat command line. Default is { "-t", "200", "-v", "time" } for:
      * </p>
      * 
      * <pre>
@@ -206,39 +226,32 @@ public @interface ReportsCrashes {
      * </pre>
      * 
      * <p>
-     * Do not include -b arguments for buffer selection, use
-     * {@link #includeEventsLogcat()} and {@link #includeRadioLogcat()} to
-     * activate alternative logcat buffers reporting. They will use the same
-     * other arguments as those provided here.
+     * Do not include -b arguments for buffer selection, use {@link #includeEventsLogcat()} and
+     * {@link #includeRadioLogcat()} to activate alternative logcat buffers reporting. They will use the same other
+     * arguments as those provided here.
      * </p>
      * 
      * <p>
-     * See <a href=
-     * "http://developer.android.com/intl/fr/guide/developing/tools/adb.html#logcatoptions"
-     * >Listing of logcat Command Options</a>.
+     * See <a href= "http://developer.android.com/intl/fr/guide/developing/tools/adb.html#logcatoptions" >Listing of
+     * logcat Command Options</a>.
      * </p>
      */
     String[] logcatArguments() default { "-t", "200", "-v", "time" };
 
     /**
-     * A special String value to allow the usage of a pseudo-null default value
-     * in annotation parameters.
+     * A special String value to allow the usage of a pseudo-null default value in annotation parameters.
      */
     public static final String NULL_VALUE = "ACRA-NULL-STRING";
 
     /**
-     * When using the {@link #formUri()} parameter to send reports to a custom
-     * server-side script, you can set here and in
-     * {@link #formUriBasicAuthPassword()} the credentials for a BASIC HTTP
-     * authentication.
+     * When using the {@link #formUri()} parameter to send reports to a custom server-side script, you can set here and
+     * in {@link #formUriBasicAuthPassword()} the credentials for a BASIC HTTP authentication.
      */
     String formUriBasicAuthLogin() default NULL_VALUE;
 
     /**
-     * When using the {@link #formUri()} parameter to send reports to a custom
-     * server-side script, you can set here and in
-     * {@link #formUriBasicAuthLogin()} the credentials for a BASIC HTTP
-     * authentication.
+     * When using the {@link #formUri()} parameter to send reports to a custom server-side script, you can set here and
+     * in {@link #formUriBasicAuthLogin()} the credentials for a BASIC HTTP authentication.
      */
     String formUriBasicAuthPassword() default NULL_VALUE;
 
@@ -249,40 +262,35 @@ public @interface ReportsCrashes {
 
     static final ReportField[] DEFAULT_REPORT_FIELDS = { REPORT_ID, APP_VERSION_CODE, APP_VERSION_NAME, PACKAGE_NAME,
             FILE_PATH, PHONE_MODEL, BRAND, PRODUCT, ANDROID_VERSION, BUILD, TOTAL_MEM_SIZE, AVAILABLE_MEM_SIZE,
-            CUSTOM_DATA, STACK_TRACE, INITIAL_CONFIGURATION, CRASH_CONFIGURATION, DISPLAY, USER_COMMENT, USER_EMAIL,
-            USER_APP_START_DATE, USER_CRASH_DATE, DUMPSYS_MEMINFO, DROPBOX, LOGCAT, EVENTSLOG, RADIOLOG, DEVICE_ID,
-            INSTALLATION_ID, DEVICE_FEATURES, ENVIRONMENT, SETTINGS_SYSTEM, SETTINGS_SECURE };
+            CUSTOM_DATA, IS_SILENT, STACK_TRACE, INITIAL_CONFIGURATION, CRASH_CONFIGURATION, DISPLAY, USER_COMMENT,
+            USER_EMAIL, USER_APP_START_DATE, USER_CRASH_DATE, DUMPSYS_MEMINFO, DROPBOX, LOGCAT, EVENTSLOG, RADIOLOG,
+            DEVICE_ID, INSTALLATION_ID, DEVICE_FEATURES, ENVIRONMENT, SETTINGS_SYSTEM, SETTINGS_SECURE };
 
     /**
-     * Add your crash reports mailbox here if you want to send reports via
-     * email. This allows to get rid of the INTERNET permission. Reports content
-     * can be customized with {@link #reportFields()}. Default fields are:
-     * {@link ReportField#USER_COMMENT}, {@link ReportField#ANDROID_VERSION},
-     * {@link ReportField#APP_VERSION_NAME} , {@link ReportField#BRAND},
-     * {@link ReportField#PHONE_MODEL}, {@link ReportField#CUSTOM_DATA},
+     * Add your crash reports mailbox here if you want to send reports via email. This allows to get rid of the INTERNET
+     * permission. Reports content can be customized with {@link #reportFields()}. Default fields are:
+     * {@link ReportField#USER_COMMENT}, {@link ReportField#ANDROID_VERSION}, {@link ReportField#APP_VERSION_NAME} ,
+     * {@link ReportField#BRAND}, {@link ReportField#PHONE_MODEL}, {@link ReportField#CUSTOM_DATA},
      * {@link ReportField#STACK_TRACE}
      */
     String mailTo() default "";
 
     /**
-     * Default list of {@link ReportField}s to be sent in email reports {@see
-     * #mailTo()}. You can set your own list with {@link #reportFields()}.
+     * Default list of {@link ReportField}s to be sent in email reports {@see #mailTo()}. You can set your own list with
+     * {@link #reportFields()}.
      */
     final static ReportField[] DEFAULT_MAIL_REPORT_FIELDS = { ReportField.USER_COMMENT, ReportField.ANDROID_VERSION,
             ReportField.APP_VERSION_NAME, ReportField.BRAND, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA,
             ReportField.STACK_TRACE };
 
     /**
-     * Controls whether unapproved reports are deleted on application start or
-     * not. Default is true. This is a change from versions of ACRA before 3.2
-     * as in NOTIFICATION mode reports were previously kept until the user
-     * explicitly opens the Notification dialog AND choose to send or discard
-     * the report. Until then, on application restart, ACRA was issuing a new
-     * crash notification for previous reports pending for approval. This could
-     * be misunderstood by the user with a new crash, resulting in bad
-     * appreciation of the application.
+     * Controls whether unapproved reports are deleted on application start or not. Default is true. This is a change
+     * from versions of ACRA before 3.2 as in NOTIFICATION mode reports were previously kept until the user explicitly
+     * opens the Notification dialog AND choose to send or discard the report. Until then, on application restart, ACRA
+     * was issuing a new crash notification for previous reports pending for approval. This could be misunderstood by
+     * the user with a new crash, resulting in bad appreciation of the application.
      */
     boolean deleteUnapprovedReportsOnApplicationStart() default true;
-    
+
     int socketTimeout() default 3000;
 }
