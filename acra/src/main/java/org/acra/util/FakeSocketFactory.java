5d47de64eb86022c7f4f8d1710b3d0e619f27703
Kevin Gaudin <kevin.gaudin@gmail.com>
2011-05-08 20:31:43 +0000

    Headers update

--- acra/src/main/java/org/acra/util/FakeSocketFactory.java
+++ acra/src/main/java/org/acra/util/FakeSocketFactory.java
@@ -1,3 +1,6 @@
+/*
+ * Utility class copied from http://transdroid.googlecode.com with the authorization from Eric Kok to redistribute it under Apache Software License. 
+ */
 package org.acra.util;
 
 import java.io.IOException;
@@ -21,26 +24,25 @@ public class FakeSocketFactory implements SocketFactory, LayeredSocketFactory {
     private SSLContext sslcontext = null;
 
     private static SSLContext createEasySSLContext() throws IOException {
-            try {
-                    SSLContext context = SSLContext.getInstance("TLS");
-                    context.init(null, new TrustManager[] { new NaiveTrustManager() }, null);
-                    return context;
-            } catch (Exception e) {
-                    throw new IOException(e.getMessage());
-            }
+        try {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new TrustManager[] { new NaiveTrustManager() }, null);
+            return context;
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
     }
 
     private SSLContext getSSLContext() throws IOException {
-            if (this.sslcontext == null) {
-                    this.sslcontext = createEasySSLContext();
-            }
-            return this.sslcontext;
+        if (this.sslcontext == null) {
+            this.sslcontext = createEasySSLContext();
+        }
+        return this.sslcontext;
     }
 
-	@Override
-	public Socket connectSocket(Socket sock, String host, int port,
-			InetAddress localAddress, int localPort, HttpParams params) throws IOException,
-			UnknownHostException, ConnectTimeoutException {
+    @Override
+    public Socket connectSocket(Socket sock, String host, int port, InetAddress localAddress, int localPort,
+            HttpParams params) throws IOException, UnknownHostException, ConnectTimeoutException {
         int connTimeout = HttpConnectionParams.getConnectionTimeout(params);
         int soTimeout = HttpConnectionParams.getSoTimeout(params);
 
@@ -48,34 +50,33 @@ public class FakeSocketFactory implements SocketFactory, LayeredSocketFactory {
         SSLSocket sslsock = (SSLSocket) ((sock != null) ? sock : createSocket());
 
         if ((localAddress != null) || (localPort > 0)) {
-                // we need to bind explicitly
-                if (localPort < 0) {
-                        localPort = 0; // indicates "any"
-                }
-                InetSocketAddress isa = new InetSocketAddress(localAddress,
-                                localPort);
-                sslsock.bind(isa);
+            // we need to bind explicitly
+            if (localPort < 0) {
+                localPort = 0; // indicates "any"
+            }
+            InetSocketAddress isa = new InetSocketAddress(localAddress, localPort);
+            sslsock.bind(isa);
         }
 
         sslsock.connect(remoteAddress, connTimeout);
         sslsock.setSoTimeout(soTimeout);
         return sslsock;
-	}
+    }
 
-	@Override
-	public Socket createSocket() throws IOException {
-		return getSSLContext().getSocketFactory().createSocket();
-	}
+    @Override
+    public Socket createSocket() throws IOException {
+        return getSSLContext().getSocketFactory().createSocket();
+    }
 
-	@Override
-	public boolean isSecure(Socket arg0) throws IllegalArgumentException {
-		return true;
-	}
+    @Override
+    public boolean isSecure(Socket arg0) throws IllegalArgumentException {
+        return true;
+    }
 
-	@Override
-	public Socket createSocket(Socket socket, String host, int port, boolean autoClose)
-			throws IOException, UnknownHostException {
-		return getSSLContext().getSocketFactory().createSocket(socket, host, port, autoClose);
-	}
+    @Override
+    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException,
+            UnknownHostException {
+        return getSSLContext().getSocketFactory().createSocket(socket, host, port, autoClose);
+    }
 
 }
