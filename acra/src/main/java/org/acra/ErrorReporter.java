a9a7896bd2ee6d73c5602577caeb2bb567d82eba
Kevin Gaudin <kevin.gaudin@gmail.com>
2011-05-08 21:11:34 +0000

    Prevent ACRA from sending report duplicates when sending silent reports in simultaneous threads.

--- acra/src/main/java/org/acra/ErrorReporter.java
+++ acra/src/main/java/org/acra/ErrorReporter.java
@@ -16,7 +16,38 @@
 package org.acra;
 
 import static org.acra.ACRA.LOG_TAG;
-import static org.acra.ReportField.*;
+import static org.acra.ReportField.ANDROID_VERSION;
+import static org.acra.ReportField.APP_VERSION_CODE;
+import static org.acra.ReportField.APP_VERSION_NAME;
+import static org.acra.ReportField.AVAILABLE_MEM_SIZE;
+import static org.acra.ReportField.BRAND;
+import static org.acra.ReportField.BUILD;
+import static org.acra.ReportField.CRASH_CONFIGURATION;
+import static org.acra.ReportField.CUSTOM_DATA;
+import static org.acra.ReportField.DEVICE_FEATURES;
+import static org.acra.ReportField.DEVICE_ID;
+import static org.acra.ReportField.DISPLAY;
+import static org.acra.ReportField.DROPBOX;
+import static org.acra.ReportField.DUMPSYS_MEMINFO;
+import static org.acra.ReportField.ENVIRONMENT;
+import static org.acra.ReportField.EVENTSLOG;
+import static org.acra.ReportField.FILE_PATH;
+import static org.acra.ReportField.INITIAL_CONFIGURATION;
+import static org.acra.ReportField.INSTALLATION_ID;
+import static org.acra.ReportField.IS_SILENT;
+import static org.acra.ReportField.LOGCAT;
+import static org.acra.ReportField.PACKAGE_NAME;
+import static org.acra.ReportField.PHONE_MODEL;
+import static org.acra.ReportField.PRODUCT;
+import static org.acra.ReportField.RADIOLOG;
+import static org.acra.ReportField.REPORT_ID;
+import static org.acra.ReportField.SETTINGS_SECURE;
+import static org.acra.ReportField.SETTINGS_SYSTEM;
+import static org.acra.ReportField.STACK_TRACE;
+import static org.acra.ReportField.TOTAL_MEM_SIZE;
+import static org.acra.ReportField.USER_COMMENT;
+import static org.acra.ReportField.USER_CRASH_DATE;
+import static org.acra.ReportField.USER_EMAIL;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -904,7 +935,7 @@ public class ErrorReporter implements Thread.UncaughtExceptionHandler {
      *            Send only reports explicitly declared as SILENT by the
      *            developer (sent via {@link #handleSilentException(Throwable)}.
      */
-    void checkAndSendReports(Context context, boolean sendOnlySilentReports) {
+    synchronized void checkAndSendReports(Context context, boolean sendOnlySilentReports) {
         File curFile = null;
         try {
             String[] reportFiles = getCrashReportFilesList();
