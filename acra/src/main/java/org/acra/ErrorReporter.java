e7f20f38fbdc1e6ebdc6cc1ac7f9816254c1ad93
William Ferguson <william.ferguson.au@gmail.com>
2012-08-02 09:20:09 +0000

    ACRA#135 Ability to remove unsent reports from an old app version. Fixed type error.

--- acra/src/main/java/org/acra/ErrorReporter.java
+++ acra/src/main/java/org/acra/ErrorReporter.java
@@ -391,7 +391,7 @@ public class ErrorReporter implements Thread.UncaughtExceptionHandler {
     private void checkReportsOnApplicationStart() {
 
         // Delete any old unsent reports if this is a newer version of the app than when we last started.
-    	final long lastVersionNr = prefs.getLong(ACRA.PREF_LAST_VERSION_NR, 0);
+    	final long lastVersionNr = prefs.getInt(ACRA.PREF_LAST_VERSION_NR, 0);
         final PackageManagerWrapper packageManagerWrapper = new PackageManagerWrapper(mContext);
         final PackageInfo packageInfo = packageManagerWrapper.getPackageInfo();
         final boolean newVersion = (packageInfo != null && packageInfo.versionCode > lastVersionNr);
