61fab77c40fb379b618ec34b55a06553e86fb35b
Diego Costantini <dcostantini@agtinternational.com>
2014-01-23 17:26:25 +0100

    added adapter to set custom socket

---
+++ src/main/java/org/acra/util/ACRAAdapter.java
@@ -0,0 +1,19 @@
+package org.acra.util;
+
+import org.apache.http.conn.scheme.SocketFactory;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
+public class ACRAAdapter {
+
+	private static SocketFactory mSocketFactory;
+
+	public static SocketFactory getSocketFactory() {
+		
+		return mSocketFactory==null? SSLSocketFactory.getSocketFactory():mSocketFactory;
+	}
+
+	public static void setSSLSocketFactory(SocketFactory factory){
+		mSocketFactory=factory;
+	}
+
+}
