c10b6dd9e3c7124578ed50d454d77195261e673e
William Ferguson <william.ferguson@xandar.com.au>
2014-08-04 14:18:30 +1000

    Better handling when no default ExceptionHandler is configured.

--- src/main/java/org/acra/ErrorReporter.java
+++ src/main/java/org/acra/ErrorReporter.java
@@ -438,7 +438,12 @@ public class ErrorReporter implements Thread.UncaughtExceptionHandler {
                         .forceCloseDialogAfterToast())) {
             // If using silent mode, let the system default handler do it's job
             // and display the force close dialog.
-            mDfltExceptionHandler.uncaughtException(brokenThread, unhandledThrowable);
+            if (mDfltExceptionHandler != null) {
+                mDfltExceptionHandler.uncaughtException(brokenThread, unhandledThrowable);
+                Log.d(ACRA.LOG_TAG, "Handing Exception on to default ExceptionHandler");
+            } else {
+                Log.w(ACRA.LOG_TAG, "NOT Handing Exception on to default ExceptionHandler - non configured");
+            }
         } else {
             // If ACRA handles user notifications with a Toast or a Notification
             // the Force Close dialog is one more notification to the user...
