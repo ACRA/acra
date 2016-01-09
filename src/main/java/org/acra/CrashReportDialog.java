674e4faae1ea652755b5a2d76fdbbca0711cbc80
William Ferguson <william.ferguson@xandar.com.au>
2016-01-09 11:55:46 +1000

    Reintroducing old CrashReportDialog so that clients that refer to it won;t crash (while trying to report a crash).

---
+++ src/main/java/org/acra/CrashReportDialog.java
@@ -0,0 +1,19 @@
+package org.acra;
+
+import android.os.Bundle;
+
+import static org.acra.ACRA.LOG_TAG;
+
+
+/**
+ * Old crash report dialog.
+ *
+ * @deprecated since 4.8.0 use {@link org.acra.dialog.CrashReportDialog} instead
+ **/
+public final class CrashReportDialog extends org.acra.dialog.CrashReportDialog {
+
+    protected void buildAndShowDialog(Bundle savedInstanceState){
+        ACRA.log.w(LOG_TAG, "org.acra.CrashReportDialog has been deprecated. Please use org.acra.dialog.CrashReportDialog instead");
+        super.buildAndShowDialog(savedInstanceState);
+    }
+}
