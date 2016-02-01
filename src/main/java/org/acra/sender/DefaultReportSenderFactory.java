c76159296ab8e8c458af6b1bc9dd500264be28b2
William Ferguson <william.ferguson@xandar.com.au>
2016-02-01 16:52:49 +1000

    Logging ACRA delivery method at INFO info instead of ERROR.

--- src/main/java/org/acra/sender/DefaultReportSenderFactory.java
+++ src/main/java/org/acra/sender/DefaultReportSenderFactory.java
@@ -37,7 +37,7 @@ public final class DefaultReportSenderFactory implements ReportSenderFactory {
             return new NullSender();
         } else if (config.formUri() != null && !"".equals(config.formUri())) {
             // If formUri is set, instantiate a sender for a generic HTTP POST form with default mapping.
-            ACRA.log.w(LOG_TAG, context.getPackageName() + " reports will be sent by Http.");
+            ACRA.log.i(LOG_TAG, context.getPackageName() + " reports will be sent by Http.");
             return new HttpSenderFactory().create(context, config);
         } else {
             return new NullSender();
