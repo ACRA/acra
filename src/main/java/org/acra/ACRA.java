ea49c2fc4664453cf5e52d2c7788826dbc6697fa
F43nd1r <lukas.morawietz@gmail.com>
2016-06-14 18:32:59 +0200

    remove unnecessary doubled catch block

--- src/main/java/org/acra/ACRA.java
+++ src/main/java/org/acra/ACRA.java
@@ -140,11 +140,7 @@ public final class ACRA {
             log.e(LOG_TAG, "ACRA#init(Application) called but no ReportsCrashes annotation on Application " + app.getPackageName());
             return;
         }
-        try {
-            init(app, new ConfigurationBuilder(app).build());
-        } catch (ACRAConfigurationException e) {
-            log.w(LOG_TAG, "Configuration Error - ACRA not started : " + e.getMessage());
-        }
+        init(app, new ConfigurationBuilder(app));
     }
 
     /**
