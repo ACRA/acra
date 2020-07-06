2bcdea98ca9c08c3e857efba91112709d54f8f44
f43nd1r <lukas.morawietz@gmail.com>
2020-07-06 19:34:33 +0200

    fix javadoc generation when $ is in text

--- annotationprocessor/src/main/java/org/acra/processor/element/AnnotationField.java
+++ annotationprocessor/src/main/java/org/acra/processor/element/AnnotationField.java
@@ -77,7 +77,7 @@ abstract class AnnotationField extends AbstractElement implements TransformedFie
     public void configureSetter(@NonNull MethodSpec.Builder builder) {
         if (javadoc != null) {
             final String name = builder.build().parameters.get(0).name;
-            builder.addJavadoc(javadoc.replaceAll("(\n|^) ", "$1").replaceAll("@return ((.|\n)*)$", "@param " + name + " $1@return this instance\n"));
+            builder.addJavadoc("$L", javadoc.replaceAll("(\n|^) ", "$1").replaceAll("@return ((.|\n)*)$", "@param " + name + " $1@return this instance\n"));
         }
     }
 
