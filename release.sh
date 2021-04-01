c8584db8c9f2a1894c1a7afdc930764d78d1776d
f43nd1r <account+github@faendir.com>
2021-04-01 11:56:23 +0200

    include current version in release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Type version to release"
+echo "Type version to release [Current version: $(./gradlew version --no-daemon --quiet --console=plain -Dorg.gradle.jvmargs=-Xmx4g)]"
 read -r version
 git tag -a "acra-$version" -m "Create version $version"
 git push --tags
