6e012a60324f5228a16a14e57c75141decd9dc5d
f43nd1r <account+github@faendir.com>
2021-04-12 02:47:12 +0200

    update gradle to be able to move release config to precompiled script plugin

--- buildSrc/src/main/kotlin/acra-release.gradle.kts
+++ buildSrc/src/main/kotlin/acra-release.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("fr.brouillard.oss.gradle.jgitver")
+    id("io.github.gradle-nexus.publish-plugin")
 }
+//apply(plugin = "io.github.gradle-nexus.publish-plugin")
 
 jgitver {
     regexVersionTag = "acra-([0-9]+(?:\\.[0-9]+){0,2}(?:-[a-zA-Z0-9\\-_]+)?)"
@@ -14,10 +16,24 @@ tasks.register("publish") {
     group = "publishing"
     subprojects {
         tasks.findByName("publish")?.let { dependsOn(it) }
+        tasks.findByName("publishToSonatype")?.let { dependsOn(it) }
     }
+    dependsOn("closeAndReleaseSonatypeStagingRepository")
 }
 
 tasks.register<Delete>("clean") {
     group = "build"
     delete = setOf(buildDir)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(project.findProperty("ossrhUser") as? String ?: System.getenv("OSSRH_USER"))
+            password.set(project.findProperty("ossrhPassword") as? String ?: System.getenv("OSSRH_PASSWORD"))
+        }
+    }
+}
+
+tasks.named("publish") {
 }
