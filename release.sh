502cca5a50b0035aa40db8feb0b70835fe8e140d
f43nd1r <account+github@faendir.com>
2021-04-13 16:14:40 +0200

    switch back to ad-m push action for javadoc upload

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Type version to release [Current version: $(git describe --abbrev=0)]"
+echo "Type version to release [Current version: $(git describe --abbrev=0 | sed -e "s/^acra-//")]"
 read -r version
 git tag -a "acra-$version" -m "Create version $version"
 git push --tags
